0cfa4a9fa0fe2016436fcd7d5c0ffab1bef4f2a6
Victor Ishii <ishii.v.4@gmail.com>
2024-08-07 17:18:07 -0300

    Logic done

--- src/BaterPonto.tsx
+++ src/BaterPonto.tsx
@@ -1,28 +1,94 @@
-import dayjs from "dayjs";
 import Expediente from "./components/Expediente";
+import Intervalo from "./components/Intervalo";
+import dayjs from 'dayjs';
 import { useState } from 'react';
 
 const BaterPonto: React.FC = ()=>{
-	const [intervalo, setIntervalo] = useState({});
+	const [showInicio, setShowInicio] = useState(true);
+	const [showIntervalo, setShowIntervalo] = useState(true);
+	const [tempoExpediente, setTempoExpediente] = useState(0);
+	const [tempoIntervalo, setTempoIntervalo] = useState(0);
+	const [entrada, setEntrada] = useState({
+		hora: 0,
+		minuto: 0,
+		segundo: 0,
+	});
+	const [intervalo, setIntervalo] = useState({
+		hora: 0,
+		minuto: 0,
+		segundo: 0,
+	});
 
-	// // const fromtime = '11:20'
-	// const totime = '12:30'
-	// const teste = dayjs();
+	const resetar = () => {
+		setEntrada({
+			...entrada,
+			hora: 0,
+			minuto: 0,
+			segundo: 0,
+		});
+		setIntervalo({
+			...intervalo,
+			hora: 0,
+			minuto: 0,
+			segundo: 0,
+		});
+		setShowInicio(true);
+		setShowIntervalo(true);
+		console.clear() 
+		setTempoIntervalo(0);
+		setTempoExpediente(0);
+	};
+
+	const valores = () => {
+		console.log(entrada);
+		console.log(intervalo);
+		console.log(tempoIntervalo);
+	};
+
+
+	const handleStart = () => {
+		setShowInicio(!showInicio);
+		setEntrada({
+			...entrada,
+			hora: parseInt(dayjs().format('H')),
+			minuto: parseInt(dayjs().format('m')),
+			segundo: parseInt(dayjs().format('s')),
+		});
+  };
+
+	const handleFinish = () => {
+		setTempoExpediente((3600 * (parseInt(dayjs().format('H')) - entrada.hora)) +
+											(60 * (parseInt(dayjs().format('m')) - entrada.minuto)) +
+											(parseInt(dayjs().format('s')) - entrada.segundo));
+		setShowInicio(!showInicio);
+		console.log(parseInt(dayjs().format('s')) - entrada.segundo);
+  };
+
+	const handleIntervaloInicio = () => {
+		setShowIntervalo(!showIntervalo);
+		setIntervalo({
+			...intervalo,
+			hora: parseInt(dayjs().format('H')),
+			minuto: parseInt(dayjs().format('m')),
+			segundo: parseInt(dayjs().format('s')),
+		});
+  };
+
+	const handleIntervaloTermino = () => {
+		setTempoIntervalo((3600 * (parseInt(dayjs().format('H')) - intervalo.hora)) +
+											(60 * (parseInt(dayjs().format('m')) - intervalo.minuto)) +
+											(parseInt(dayjs().format('s')) - intervalo.segundo));
+		setShowIntervalo(!showIntervalo);
+  };
 	
-	// const ft = dayjs(`2000-01-01 11:20`);
-	// const tt = dayjs(`2000-01-01 ${totime}`);
-	// const mins = tt.diff(ft, "minutes", true);
-	// // const totalHours = parseInt(mins / 60);
-	// // const totalMins = dayjs().minute(mins).$m
-	// console.log(mins);
-	// console.log(teste);
-	// // console.log(totalMins);
-	return(
+  return (
 		<div>
-			<Expediente/>
-			
+			<button onClick={resetar}>Reset</button>
+			<button onClick={valores}>Ver valores</button>
+			<Expediente handleStart = {handleStart} handleFinish = {handleFinish} showInicio = {showInicio} tempoExpediente = {tempoExpediente}/>
+			<Intervalo handleIntervaloInicio = {handleIntervaloInicio} handleIntervaloTermino = {handleIntervaloTermino} showInicio = {showInicio} tempoIntervalo={tempoIntervalo} showIntervalo = {showIntervalo}/>
 		</div>
-	);
+  );
 };
 
 export default BaterPonto;
--- src/components/Expediente.tsx
+++ src/components/Expediente.tsx
@@ -1,97 +1,33 @@
-import dayjs from 'dayjs';
-import { useState } from 'react';
 import { TimerAsFC } from './Timer';
 
-const Expediente: React.FC = () => {
-	const [showInicio, setShowInicio] = useState(true);
-	const [showIntervalo, setShowIntervalo] = useState(true);
-	const [entrada, setEntrada] = useState({
-		hora: 0,
-		minuto: 0,
-		segundo: 0,
-	});
-	const [intervalo, setIntervalo] = useState({
-		hora: 0,
-		minuto: 0,
-		segundo: 0,
-	});
+interface ChildProps {
+  handleStart: () => void;
+	handleFinish: () => void;
+	showInicio: boolean;
+	tempoExpediente: number;
+}
+
+export const Expediente: React.FC<ChildProps> = ({
+  handleStart,
+	handleFinish,
+	showInicio,
+	tempoExpediente
+}) => {
 	let content;
-	let content_almoco;
+	let texto_expediente;
 
-	const resetar = () => {
-		setEntrada({
-			...entrada,
-			hora: 0,
-			minuto: 0,
-			segundo: 0,
-		});
-		setIntervalo({
-			...entrada,
-			hora: 0,
-			minuto: 0,
-			segundo: 0,
-		});
-		setShowInicio(true);
-		setShowIntervalo(true);
-		console.clear() 
-	};
-
-	const valores = () => {
-		console.log(entrada);
-		console.log(intervalo);
-	};
-
-
-	const handleStart = () => {
-		setShowInicio(!showInicio);
-		setEntrada({
-			...entrada,
-			hora: parseInt(dayjs().format('H')),
-			minuto: parseInt(dayjs().format('m')),
-			segundo: parseInt(dayjs().format('s')),
-		});
-  };
-
-	const handleFinish = () => {
-		setShowInicio(!showInicio);
-		console.log(parseInt(dayjs().format('s')) - entrada.segundo);
-		// console.log(parseInt(dayjs().format('m')) - entrada.minuto);
-  };
-
-	const handleIntervalo = () => {
-		setShowIntervalo(!showIntervalo);
-		setIntervalo({
-			...intervalo,
-			hora: parseInt(dayjs().format('H')),
-			minuto: parseInt(dayjs().format('m')),
-			segundo: parseInt(dayjs().format('s')),
-		});
-  };
-
-	const handleTermino = () => {
-		setShowIntervalo(!showIntervalo);
-		console.log(parseInt(dayjs().format('s')) - intervalo.segundo);
-		// console.log(parseInt(dayjs().format('m')) - intervalo.minuto);
-  };
-
-	if (showInicio)
-		content = <TimerAsFC handleTimer = {handleStart}>Começar expediente </TimerAsFC>
-	else
-		content = <TimerAsFC handleTimer = {handleFinish}> Terminar expediente </TimerAsFC>
-
-	if (!showInicio) {
-		if (showIntervalo)
-			content_almoco = <TimerAsFC handleTimer = {handleIntervalo}> Começar intervalo </TimerAsFC>
+	if (tempoExpediente === 0)
+		if (showInicio)
+			content = <TimerAsFC handleTimer = {handleStart}>Começar expediente </TimerAsFC>
 		else
-			content_almoco = <TimerAsFC handleTimer = {handleTermino}> Terminar intervalo </TimerAsFC>
-	}	
+			content = <TimerAsFC handleTimer = {handleFinish}> Terminar expediente </TimerAsFC>
+	else
+		texto_expediente = <div>Tempo do expediente foi {Math.floor(tempoExpediente/3600)} horas, {Math.floor(tempoExpediente/60)} minutos e {tempoExpediente%60} segundos</div>
 
   return (
 		<div>
-			<button onClick={resetar}>Reset</button>
-			<button onClick={valores}>Ver valores</button>
 			{content}
-			{content_almoco}
+			{texto_expediente}
 		</div>
   );
 };
