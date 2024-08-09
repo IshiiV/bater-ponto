ccb0ce7f52605195c30161f4545ecb04393658e7
Victor Ishii <ishii.v.4@gmail.com>
2024-08-09 11:57:47 -0300

    last touches

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -15,15 +15,15 @@ export const TimerAsFC: React.FC<ChildProps> = ({
 }) => {
 	const classes =
 		className(
-			' items-center px-3 py-1.5 border',
+			'w-96 rounded  text-center items-center px-3 py-1.5 border text-xl',
 			{
-				'text-center flex-auto rounded border-blue-500 bg-blue-500 text-white': comeco,
-				'text-center flex-auto rounded border-red-500 bg-red-500 text-white': termino,
+				'border-blue-500 bg-blue-500 text-white': comeco,
+				'border-red-500 bg-red-500 text-white': termino,
 			}
 	);
 
   return (
-		<div className="flex">
+		<div className="flex justify-center">
 			<button className={classes} onClick = {handleTimer}>{children}</button>
 		</div>
   );
